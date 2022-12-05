4a4374ccb809bc7b4e863458ad0d56cbccfbacc6
Michał Oręziak <micorix.dev@gmail.com>
2022-12-05 22:42:28 +0100

    Don't use CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi_pagination import add_pagination
 from app.api.api_router import api_router
 from app.sentry import setup_sentry
@@ -7,14 +6,6 @@ from app.sentry import setup_sentry
 setup_sentry()
 
 app = FastAPI(title="po8klasie-fastapi")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(api_router, prefix="/api")
 
