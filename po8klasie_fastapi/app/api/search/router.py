7828ea957b5c20788c183c1c009d0317cf2926be
Michał Oręziak <micorix.dev@gmail.com>
2023-04-22 17:14:30 +0200

    Scope autocomplete to project

--- po8klasie_fastapi/app/api/search/router.py
+++ po8klasie_fastapi/app/api/search/router.py
@@ -36,7 +36,7 @@ search_router.include_router(search_map_features_router, prefix="/map_features")
 
 @search_router.get("/autocomplete")
 def route_search_autocomplete(
-    query: str = Query(default=...), project_id: str = "", db: Session = Depends(get_db)
+        query: str = Query(default=...), project_id: str = Query(default=...), db: Session = Depends(get_db)
 ):
     institutions = query_secondary_school_institutions(
         db,
@@ -48,9 +48,7 @@ def route_search_autocomplete(
     )
 
     institutions = filter_by_query(institutions, query)
-
-    if project_id:
-        institutions = filter_by_project_id(institutions, project_id)
+    institutions = filter_by_project_id(institutions, project_id)
 
     return institutions.limit(5).all()
 
@@ -79,8 +77,8 @@ def route_search_single_institution(rspo=Required, db: Session = Depends(get_db)
 
 @search_router.get("/institution")
 def route_search_institutions(
-    filters_query: Annotated[FiltersQuery, Depends(FiltersQuery)],
-    db: Session = Depends(get_db),
+        filters_query: Annotated[FiltersQuery, Depends(FiltersQuery)],
+        db: Session = Depends(get_db),
 ):
     institutions = filter_institutions(db, filters_query.model)
     institutions = order_institutions(institutions)
